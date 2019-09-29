976119a70afd0b5ce672767ea8d6f149d83f6d6d
Sam <samanby5@gmail.com>
2019-09-29 12:54:57 -0400

    adding Authentication

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, {Component} from 'react';
 //import {UserForm} from './components/Registration/UserForm';
-import LoginHandler  from './components/Login/LoginHandler';
-import { BrowserRouter as Router, Route } from "React-router-dom";
-
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from './Auth';
 import './App.css';
+import LoginHandler  from './components/Login/LoginHandler';
+import LoginScreen from './components/Login/LoginScreen';
+import UserForm from './components/Registration/UserForm';
 
 class App extends Component{ 
   //It Call Login
   render() {
     return (
-      <div className="App">
-        <LoginHandler/>
-      </div>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Route exact path = "/" Component={LoginHandler} />
+            <Route exact path = "/login" Component={LoginScreen} />
+            <Route exact path = "/signup" Component={UserForm} />
+          </div>
+        </Router>
+      </AuthProvider>
   );
   }
 }
---
+++ src/Auth.js
@@ -0,0 +1,23 @@
+import React, { useEffect, userState } from 'react';
+import app from "./base.js";
+
+export const AuthContext = React.createContext();
+
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = userState(null);
+
+    useEffect(() => {
+        app.auth().onAuthStateChanged(setCurrentUser);
+    }, []);
+
+    return (
+        <AuthContext.Provider
+            value={{
+                currentUser
+            }}
+        >
+        {children}
+        </AuthContext.Provider>
+    );
+};
